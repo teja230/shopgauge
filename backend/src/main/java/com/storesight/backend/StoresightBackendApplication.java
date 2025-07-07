9c7bbdf2d6b84c07a7573a7cb9aaef093e6078c1
Raviteja Kapalavayi <teja230@gmail.com>
2025-07-06 21:53:02 -0500

    Final attempt to fix JPA EntityManagerFactory issue

--- backend/src/main/java/com/storesight/backend/StoresightBackendApplication.java
+++ backend/src/main/java/com/storesight/backend/StoresightBackendApplication.java
@@ -4,12 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JpaRepositoriesAutoConfiguration.class})
 @ComponentScan(basePackages = "com.storesight.backend")
 @EnableScheduling
 public class StoresightBackendApplication {
